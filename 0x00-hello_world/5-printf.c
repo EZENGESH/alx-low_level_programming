6c2c26cc7713da5ff3753b541e37a2d9415453f4
EZENGESH <ezengesh@gmail.com>
2022-12-08 15:13:47 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/* main-entry point
+ *
+ * print-code using funtion printf
+ *
+ * return: always 0 (success)
+ */
+
+int	main(void)
+{
+	prinf("\"with proper grammar, but the outcome is a piece of art");
+
+	return (0);
+}
+
