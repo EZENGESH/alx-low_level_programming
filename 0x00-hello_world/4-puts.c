4658f1199c49efd69dada101e8155fe2f2e037ab
EZENGESH <ezengesh@gmail.com>
2022-12-08 14:02:45 -0800

    print using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
  * main print using puts function
- * return 0 if exited properly	
+ * return 0 if exited properly
  */
 
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
