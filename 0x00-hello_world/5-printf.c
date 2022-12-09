2ee84f63593d95465922edfc3336aba8d54225c7
EZENGESH <ezengesh@gmail.com>
2022-12-08 16:08:54 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
-/* main-entry point
+/**
+ * main-entry point
  *
  * print-code using funtion printf
  *
@@ -9,7 +10,7 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
