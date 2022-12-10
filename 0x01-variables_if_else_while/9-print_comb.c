5ec6b1a6d94ff2d6370e8d11ee77e0347056a2e1
EZENGESH <ezengesh@gmail.com>
2022-12-10 10:24:14 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (1 = 0; i < 10; i++) {
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	puthchar('\n');
+	return (0);
+}
