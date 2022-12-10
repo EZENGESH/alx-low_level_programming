ee7f65777095dd423a8325fbe61359b73cd000b4
EZENGESH <ezengesh@gmail.com>
2022-12-10 10:07:47 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	int i;
+
+	c = 'a';
+	while
+		(i < 10) {
+			putchar(i + '0');
+			i++;
+		}
+	while
+		(c < 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
