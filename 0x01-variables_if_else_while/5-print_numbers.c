c72851ca2e4f9213ddd60c490450104f590266b1
EZENGESH <ezengesh@gmail.com>
2022-12-09 21:58:16 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while
+		(i<10) {
+			putchar(i);
+			i++
+		}
+	puchar('\n');
+	return (0);
+}
