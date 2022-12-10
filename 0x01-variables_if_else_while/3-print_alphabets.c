8ce3b577ef4c1b076943dc6a83558bebcd913501
EZENGESH <ezengesh@gmail.com>
2022-12-09 20:48:19 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(voi)
+{
+	char c;
+
+	char i;
+
+	c = 'a';
+	i = 'A';
+	while
+		(c = 'z') {
+			putchar(c);
+			c++;
+		}
+	while (c = 'Z') {
+			putchar(i);
+			i++;
+		}
+	putchar('\n');
+	return (0);
+
+
+
