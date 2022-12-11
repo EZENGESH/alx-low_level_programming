d2f4f451ca78ae325349b5b6416a4af5f839ebdf
EZENGESH <ezengesh@gmail.com>
2022-12-11 13:18:00 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a , b;
+
+	for (a = 0; a <= 98; a++)
+	{
+		for (b = c + 1; b <= 99; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((a / 10) + '0');
+
+			putchar(' ');
+
+			putchar((b % 10) + '0');
+			putchar((b / 10) + '0');
+
+			if (a == 89 && b == 99)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+
