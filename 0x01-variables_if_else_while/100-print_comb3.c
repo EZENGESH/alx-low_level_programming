b92164be9a479d2b417cc3db6e1fffc9fe78d4f9
EZENGESH <ezengesh@gmail.com>
2022-12-11 05:41:18 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,6 +9,7 @@ int main(void)
 {
 	int ones = n /10;	
 	int tens = n % 10;
+	int n;
 	for (ones = 0; ones < 10; ones++)
 	{
 		for (tens = 10; tens < 100; tens++)
@@ -16,8 +17,7 @@ int main(void)
 			if (ones < 10 || tens > ones)
 			{
 				putchar(n + '0');
-						
-					if (n < 10 && n < 99))
+				if (n < 10 && n < 99)
 				{
 					putchar(',');
 					putchar(' ');
