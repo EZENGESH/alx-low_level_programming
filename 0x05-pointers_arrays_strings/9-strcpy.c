adb35ec9c2d2b3dbd777a3a80991153d5f8903d1
EZENGESH <ezengesh@gmail.com>
2022-12-20 07:10:27 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - copy paste string
+ * @dest: destination
+ * @src: source
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+	
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
+			
