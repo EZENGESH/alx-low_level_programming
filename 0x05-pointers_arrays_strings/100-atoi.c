a74a28ef74dd22e95348c05b5f75e214be1ff4cb
EZENGESH <ezengesh@gmail.com>
2022-12-20 07:46:00 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,7 +4,7 @@
  * @s: the string to be converted.
  * Return: The integer value of the converted string
  */
-nt _atoi(char *s)
+int _atoi(char *s)
 {
 	int sign;
 	unsigned int num;
@@ -13,13 +13,13 @@ nt _atoi(char *s)
 	temp = s;
 	num = 0;
 	sign = 1;
-	while (*temp !='\0' && (*temp < '0' || *temp > '9'))
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
 		if (*temp == '-')
 			sign *= -1;
 		temp++;
 	}
-	if (*temp !'\0')
+	if (*temp != '\0')
 	{
 		do {
 			num =num * 10 + (*temp - '0');
@@ -28,4 +28,3 @@ nt _atoi(char *s)
 	}
 	return(num * sign);
 }
-
