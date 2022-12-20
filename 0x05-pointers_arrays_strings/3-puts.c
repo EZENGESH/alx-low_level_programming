abe24f0af0f8c915aa419fab5e65e96a31ed4a12
EZENGESH <ezengesh@gmail.com>
2022-12-19 23:55:17 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _puts - write a function that print a string, follwed by a new line to 
+ * stdout.
+ *
+ * @str: this my string
+ *
+ */
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
